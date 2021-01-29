84bd9b8213298166bf262736234a9f6c048af084
Uygar KAYA <73105132+UygarKAYA@users.noreply.github.com>
2021-01-29 16:14:15 +0300

    BinaryTreeNode.h is created

---
+++ Binary Search Tree/BinaryTreeNode.h
@@ -0,0 +1,13 @@
+#ifndef _BinaryTreeNode_
+#define _BinaryTreeNode_
+
+class BinaryTreeNode 
+{
+	public:
+		BinaryTreeNode(int);
+		int element;
+		BinaryTreeNode* left;
+		BinaryTreeNode* right;
+};
+
+#endif
